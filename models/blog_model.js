005b812b02a9ee6cefeb1c8f3f80f59d74bbf056
shuvojit007 <shuvojitkar241@gmail.com>
2018-05-09 12:00:53 +0600

    Bug fixed

--- models/blog_model.js
+++ models/blog_model.js
@@ -14,10 +14,7 @@ const BlogSchema = new Schema( {
         type: Schema.Types.ObjectId,
         ref: 'user'
     },
-    image: {
-        type: String,
-        default: "default"
-    },
+    image: String,
     comments: [{
         type: Schema.Types.ObjectId,
         ref: 'comments'
